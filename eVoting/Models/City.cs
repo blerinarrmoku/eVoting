f27aae7dfcad7c9989df80318c2732cbb766d13e
Blerina Rrmoku <blerina@gjirafa.com>
2022-02-01 15:37:23 +0100

    Add project files.

---
+++ eVoting/Models/City.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace eVoting.App.Models
+{
+    public partial class City
+    {
+        public City()
+        {
+            Parties = new HashSet<Party>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime? InsertedDateTime { get; set; }
+        public DateTime? UpdatedDateTime { get; set; }
+
+        public virtual ICollection<Party> Parties { get; set; }
+    }
+}
